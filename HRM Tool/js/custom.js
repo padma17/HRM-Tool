d81ec72cc1f74f225a568b5d3572820ce26ed08c
Padma Ravi <padmaravi093@gmail.com>
2019-03-06 12:29:04 +0530

    Update custom.js
    Missing uploading some files while commiting. Uploading them now.

--- HRM Tool/js/custom.js
+++ HRM Tool/js/custom.js
@@ -15,14 +15,16 @@ $.ajax({
 	+rowData.email+'</td><td>'+rowData.birth_date+'</td><td>'+rowData.year_of_experience+'</td><td>'
 	+rowData.position_applied+'</td><td>'+rowData.application_date+'</td><td>'+rowData.status+'</td></tr>');
 }
-$("#hrtable" ).DataTable({
+var table = $("#hrtable" ).DataTable({
 	                
-                    filter:false,
+                    			filter:false,
 					aaSorting : [3, 'asc'], //Sorting of table could be performed by clicking on arrow keys/column headers. By default I have kept "Position Applied" column aligned in ascending order.Furthur,Other columns sorting can be performed by using arrow keys.
 					info:false,
-					responsive: true,
+					responsive: false,
 					paging: false,
-					"searching": true					
+					"searching": true,
+					"scrollX": true,
+					"scrollY": false,
 });
     }
     });
